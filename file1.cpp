b56d7d39566797be2cf615d619ed07495305d6ae
Marbelaezl <124514421+Marbelaezl@users.noreply.github.com>
2023-02-05 12:22:40 -0500

    Create file1.cpp
    hola mundo creo

---
+++ file1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+float funcion (int par1, int par2);
+
+int main(){
+cout<<"hola mundo pero con multiplicación"<<endl<<funcion(3,5);
+
+
+return 0;
+}
+
+float funcion (int par1, int par2){ return (par1*par2);
+// Literalmente solo multiplicación
